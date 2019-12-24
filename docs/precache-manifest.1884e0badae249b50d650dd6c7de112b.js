eba3f3fb347eb9981b09a7177e07682603cae874
ashok <ashok.g@videoken.com>
2019-12-24 13:01:00 -0600

    #added build file

---
+++ docs/precache-manifest.1884e0badae249b50d650dd6c7de112b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199d23d91c5e23aef8e09ce49fc10f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14a30deae8fba6095f2",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "495bc83db0974c004842",
+    "url": "/static/css/main.f502c476.chunk.css"
+  },
+  {
+    "revision": "d14a30deae8fba6095f2",
+    "url": "/static/js/2.dbe10059.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.dbe10059.chunk.js.LICENSE"
+  },
+  {
+    "revision": "495bc83db0974c004842",
+    "url": "/static/js/main.2c56c73f.chunk.js"
+  },
+  {
+    "revision": "9b6170541f3bb9e349e8",
+    "url": "/static/js/runtime-main.ab54baa3.js"
+  }
+]);
